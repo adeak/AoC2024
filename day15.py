e8c8ab97a76f446cda2e3b0c8a6f654da928010f
adeak <adeak@users.noreply.github.com>
2024-12-15 15:58:42 +0100

    Day 15 refactored

--- day15.py
+++ day15.py
@@ -3,9 +3,9 @@ import numpy as np
 
 class Crate:
     def __init__(self, poses):
-        self.poses = poses  # tuple of 2-tuples
+        self.poses = poses  # tuple of 2-tuples (length 1 or 2)
     
-    def try_move(self, delta, board, crates):
+    def can_move(self, delta, board, crates):
         for pos in self.poses:
             next_pos = tuple(np.array(pos) + delta)
             if board[next_pos] == '#':
@@ -18,7 +18,7 @@ class Crate:
                 if crate is self:
                     continue
                 if next_pos in crate.poses:
-                    could_move = crate.try_move(delta, board, crates)
+                    could_move = crate.can_move(delta, board, crates)
                     if not could_move:
                         # we can't move either
                         return False
@@ -36,7 +36,7 @@ class Crate:
         return children | {self}
 
 
-def day15(inp):
+def day15(inp, part1=True):
     board_str, moves = inp.strip().split('\n\n')
     moves = moves.replace('\n', '')
     board = np.array([list(line) for line in board_str.splitlines()])
@@ -47,42 +47,22 @@ def day15(inp):
         '<': [0, -1],
     }
 
-    # part 1
-    start = np.array((board == '@').nonzero()).ravel()
-    board[tuple(start)] = '.'
-    pos = start
-    for move in moves:
-        delta = deltas[move]
-        next_pos = pos + delta
-        if board[tuple(next_pos)] == '.':
-            # no-brainer move
-            pos = next_pos
-            continue
-        if board[tuple(next_pos)] == '#':
-            # no-brainer non-move
-            continue
-        bump_pos = next_pos + delta
-        while board[tuple(bump_pos)] == 'O':
-            bump_pos += delta
-        if board[tuple(bump_pos)] == '#':
-            # can't actually move
-            continue
-        # teleport first box to last position
-        board[tuple(next_pos)] = '.'
-        board[tuple(bump_pos)] = 'O'
-        pos = next_pos
-    box_x, box_y = (board == 'O').nonzero()
-    part1 = (100 * box_x + box_y).sum()
+    if part1:
+        board = np.array([list(line) for line in board_str.splitlines()])
+        crates = [
+            Crate((pos,))
+            for pos in zip(*np.array((board == 'O').nonzero()).reshape(2, -1))
+        ]
+    else:
+        board_str = board_str.replace('O', '[]').replace('#', '##').replace('.', '..').replace('@', '@.')
+        board = np.array([list(line) for line in board_str.splitlines()])
+        crates = [
+            Crate((start_pos, (start_pos[0], start_pos[1] + 1)))
+            for start_pos in zip(*np.array((board == '[').nonzero()).reshape(2, -1))
+        ]
 
-    # part 2
-    board_str = board_str.replace('O', '[]').replace('#', '##').replace('.', '..').replace('@', '@.')
-    board = np.array([list(line) for line in board_str.splitlines()])
     start = np.array((board == '@').nonzero()).ravel()
     board[tuple(start)] = '.'
-    crates = [
-        Crate((start_pos, (start_pos[0], start_pos[1] + 1)))
-        for start_pos in zip(*np.array((board == '[').nonzero()).reshape(2, -1))
-    ]
     pos = start
     for move in moves:
         delta = deltas[move]
@@ -98,7 +78,7 @@ def day15(inp):
         # there's exactly one crate in our way here
         for crate in crates:
             if next_pos in crate.poses:
-                could_move = crate.try_move(delta, board, crates)
+                could_move = crate.can_move(delta, board, crates)
                 break
         if not could_move:
             # no move
@@ -115,13 +95,13 @@ def day15(inp):
         board[tuple(shifting_inds.T)] = '.'
         board[tuple(new_inds.T)] = old_contents
         pos = next_pos
-    box_x, box_y = (board == '[').nonzero()
-    part2 = (100 * box_x + box_y).sum()
 
-    return part1, part2
+    result = sum(crate.poses[0][0] * 100 + crate.poses[0][1] for crate in crates)
+    return result
+
 
 if __name__ == "__main__":
     testinp = open('day15.testinp').read()
-    print(day15(testinp))
+    print(day15(testinp), day15(testinp, part1=False))
     inp = open('day15.inp').read()
-    print(day15(inp))
+    print(day15(inp), day15(inp, part1=False))
