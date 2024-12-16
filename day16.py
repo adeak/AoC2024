211977dc011aa347ae7241d02ecd3adcf3ca720c
adeak <adeak@users.noreply.github.com>
2024-12-16 21:46:13 +0100

    Day 16 nice

--- day16.py
+++ day16.py
@@ -25,105 +25,47 @@ def day16(inp):
     board[target] = board[start] = '.'
 
     orient = '>'
-    state = (0, start, orient)
-    paths = [state]  # heapq
-    best_scores = {(start, orient): state[0]}  # state -> best score
-    while True:
-        score, pos, orient = heappop(paths)
-        # try to step forward
-        next_pos = tuple(np.array(pos) + deltas[orient])
-        if board[next_pos] == '.':
-            next_state = (score + 1, next_pos, orient)
-            if next_pos == target:
-                # we're done with part 1
-                winning_score = next_state[0]
-                return winning_score
-
-            if next_state[0] <= best_scores.get(next_state[1:], np.inf):
-                # worth pursuing this step
-                heappush(paths, next_state)
-                best_scores[next_state[1:]] = next_state[0]
-
-        for sane_turn in sane_turns[orient]:
-            # only worth turning if step after turn is valid
-            next_pos = tuple(np.array(pos) + deltas[sane_turn])
-            if board[next_pos] == '.':
-                next_state = (score + 1001, next_pos, sane_turn)
-                if next_pos == target:
-                    # we're done
-                    winning_score = next_state[0]
-                    return winning_score
-                if next_state[0] <= best_scores.get(next_state[1:], np.inf):
-                    # worth pursuing this step
-                    heappush(paths, next_state)
-                    best_scores[next_state[1:]] = next_state[0]
-
-
-def day16b(inp, winning_score):
-    board = np.array([list(line) for line in inp.splitlines()])
-    deltas = {
-        'v': [1, 0],
-        '^': [-1, 0],
-        '>': [0, 1],
-        '<': [0, -1],
-    }
-    sane_turns = {
-        'v': '<>',
-        '^': '<>',
-        '<': 'v^',
-        '>': 'v^',
-    }
-
-    target = tuple(np.array((board == 'E').nonzero()).ravel())
-    start = tuple(np.array((board == 'S').nonzero()).ravel())
-    board[target] = board[start] = '.'
-
-    orient = '>'
     path = (0, (start,), orient)
     paths = [path]  # heapq
     best_scores = {(path[1][-1], orient): path[0]}  # (pos, orientation) -> best score
     good_tiles = set()
+    winning_score = np.inf
     while paths:
         score, tiles, orient = heappop(paths)
-        # try to step forward
-        next_pos = tuple(np.array(tiles[-1]) + deltas[orient])
-        if board[next_pos] == '.' and score + 1 <= winning_score:
-            next_state = (score + 1, tiles + (next_pos,), orient)
-            if next_pos == target:
-                # we're done with this path
-                best_scores[next_pos, orient] = winning_score
-                for tile in next_state[1]:
-                    good_tiles.add(tile)
-                # and there's no point turning anymore
-                continue
 
-            if next_state[0] <= best_scores.get((next_pos, orient), winning_score):
-                # worth pursuing this step
-                heappush(paths, next_state)
-                best_scores[next_pos, orient] = next_state[0]
-
-        for sane_turn in sane_turns[orient]:
-            # only worth turning if step after turn is valid
-            next_pos = tuple(np.array(tiles[-1]) + deltas[sane_turn])
-            if board[next_pos] == '.' and score + 1001 <= winning_score:
-                next_state = (score + 1001, tiles + (next_pos,), sane_turn)
+        # check forward step, and step after two potential meaningful turns
+        next_orientations = orient + sane_turns[orient]
+        costs = [1, 1001, 1001]
+        for next_orient, cost in zip(next_orientations, costs):
+            next_pos = tuple(np.array(tiles[-1]) + deltas[next_orient])
+            if board[next_pos] == '.' and score + cost <= winning_score:
+                next_state = (score + cost, tiles + (next_pos,), next_orient)
                 if next_pos == target:
                     # we're done with this path
-                    best_scores[next_pos, sane_turn] = winning_score
                     for tile in next_state[1]:
                         good_tiles.add(tile)
+
+                    if winning_score == np.inf:
+                        # first success, set winning score
+                        winning_score = next_state[0]
+                        # prune remaining states
+                        paths = [path for path in paths if path[0] <= winning_score]
+                        heapify(paths)
+
+                    best_scores[next_pos, next_orient] = winning_score
                     continue
 
-                if next_state[0] <= best_scores.get((next_pos, sane_turn), winning_score):
+                if next_state[0] <= best_scores.get((next_pos, next_orient), winning_score):
                     # worth pursuing this step
                     heappush(paths, next_state)
-                    best_scores[next_pos, sane_turn] = next_state[0]
-    return len(good_tiles)
+                    best_scores[next_pos, next_orient] = next_state[0]
+
+    part1 = winning_score
+    part2 = len(good_tiles)
+    return part1, part2
 
 if __name__ == "__main__":
     testinp = open('day16.testinp').read()
-    score = day16(testinp)
-    print(score, day16b(testinp, score))
+    print(day16(testinp))
     inp = open('day16.inp').read()
-    score = day16(inp)
-    print(score, day16b(inp, score))
+    print(day16(inp))
