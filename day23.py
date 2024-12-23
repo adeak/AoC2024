7ec1e89ebd694c79280613c5ecef53ef60f2c27e
adeak <adeak@users.noreply.github.com>
2024-12-23 10:00:13 +0100

    Day 23 refactor

--- day23.py
+++ day23.py
@@ -1,30 +1,18 @@
-from collections import defaultdict
-
 import networkx as nx  # sorry
 
 
 def day23(inp):
-    data = inp.splitlines()
-    connections = defaultdict(set)
-    for connection in data:
-        this, that = connection.split('-')
-        connections[this].add(that)
-        connections[that].add(this)
+    connections = inp.splitlines()
 
-    seeds = {node for node in connections if node.startswith('t')}
-    cliques = set()
-    for seed in seeds:
-        neighbs = connections[seed]
-        for neighb in neighbs:
-            next_neighbs = connections[neighb] & neighbs
-            for next_neighb in next_neighbs:
-                cliques.add(frozenset([seed, neighb, next_neighb]))
-    part1 = len(cliques)
-
-    G = nx.Graph()
-    for start, ends in connections.items():
-        G.add_edges_from([(start, end) for end in ends])
-    max_clique = max(nx.find_cliques(G), key=len)
+    G = nx.Graph([connection.split('-') for connection in connections])
+    cliques = list(nx.enumerate_all_cliques(G))
+    part1 = sum(
+        1
+        for clique in cliques
+        if len(clique) == 3
+        and any(node.startswith('t') for node in clique)
+    )
+    max_clique = max(cliques, key=len)
     part2 = ','.join(sorted(max_clique))
 
     return part1, part2
